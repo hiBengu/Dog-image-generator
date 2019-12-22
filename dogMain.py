e7e51ad3a3556c61ff8e76ba82921d6fcab00ac2
hibengu <hibrahimbengu@gmail.com>
2019-12-22 11:54:54 +0300

    add argparser

--- dogMain.py
+++ dogMain.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-#%matplotlib inline
 import argparse
 import os
 import random
@@ -26,37 +25,53 @@ print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
-dataroot = "/home/itu/hibengu/datasets/dogData" # path to datasets
-workers = 2 # number of worker threads for loading the data with DataLoader
-batchSize = 128 # DCGAN paper uses batch size of 128
-imageSize = 64
-nc = 3 # number of channels in images, for colored images = 3
-nz = 100 # size of fenerator input, latent vector
-ngf = 64 # size of feature maps in Generator
-ndf = 64 # size of fetaure maps in Discriminator
-numEpochs = 300
-lr = 0.0002 # In DCGAN paper it is stated that it should be 0.0002
-beta1 = 0.5 # hyperparameter for Adam optimizers. In paper it is said that it should be 0.5
-ngpu = 1
-showTrainData = True # Show train images
+parser = argparse.ArgumentParser(description='Generate fake dog images')
+parser.add_argument('--dataroot', default='../dogData',
+                    help='Dataset directory')
+parser.add_argument('--workers', type=int, default='2',
+                    help='number of worker thread for loading the data')
+parser.add_argument('--batchSize', type=int, default='128',
+                    help='Batch Size, DCGAN paper uses batch size of 128')
+parser.add_argument('--imageSize', type=int, default='64',
+                    help='Size of the images')
+parser.add_argument('--nc', type=int, default='3',
+                    help='number of colour channels of images')
+parser.add_argument('--nz', type=int, default='100',
+                    help='Size of the generator input')
+parser.add_argument('--ngf', type=int, default='64',
+                    help='size of feature maps in Generator')
+parser.add_argument('--ndf', type=int, default='64',
+                    help='size of feature maps in Discriminator')
+parser.add_argument('--numEpochs', type=int, default='100',
+                    help='number of epochs')
+parser.add_argument('--lr', type=float, default='0.0002',
+                    help='learning rate')
+parser.add_argument('--beta1', type=float, default='0.5',
+                    help='hyperparameter for adam optimizer')
+parser.add_argument('--ngpu', type=int, default='1',
+                    help='number of gpus that will be used')
+parser.add_argument('--showTrainData', type=int, default='1',
+                    help="0 if don't want to see train data")
+
+args = parser.parse_args()
 
 # Create dataset
-dataset = dset.ImageFolder(root=dataroot,
+dataset = dset.ImageFolder(root=args.dataroot,
                            transform=transforms.Compose([
-                               transforms.Resize(imageSize),
-                               transforms.CenterCrop(imageSize),
+                               transforms.Resize(args.imageSize),
+                               transforms.CenterCrop(args.imageSize),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
 
 # Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batchSize,
-                                         shuffle=True, num_workers=workers)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batchSize,
+                                         shuffle=True, num_workers=args.workers)
 
 # Choose device
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if (torch.cuda.is_available() and args.ngpu > 0) else "cpu")
 
-if (showTrainData):
+if (args.showTrainData != 0):
     real_batch = next(iter(dataloader))
     plt.figure(figsize=(8,8))
     plt.axis("off")
@@ -80,23 +95,23 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            nn.ConvTranspose2d(args.nz, args.ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(args.ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
+            nn.ConvTranspose2d(args.ngf * 8, args.ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
+            nn.ConvTranspose2d(args.ngf * 4, args.ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
+            nn.ConvTranspose2d(args.ngf * 2, args.ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(args.ngf, args.nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -105,10 +120,10 @@ class Generator(nn.Module):
         return self.main(input)
 
 # Create the Generator
-netG = Generator(ngpu).to(device)
+netG = Generator(args.ngpu).to(device)
 
 # Handle multi-gpu if desired
-if (device.type == 'cuda') and (ngpu > 1):
+if (device.type == 'cuda') and (args.ngpu > 1):
     netG = DataParallel(netG, list(range(ngpu)))
 
 # Apply  the weights_init function to init. weights to mean = 0, stdev = 0.2
@@ -119,25 +134,25 @@ print(netG)
 class Discriminator(nn.Module):
     def __init__(self, ngpu):
         super(Discriminator, self).__init__()
-        self.ngpu = ngpu
+        self.ngpu = args.ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(args.nc, args.ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.Conv2d(args.ndf, args.ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.Conv2d(args.ndf * 2, args.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.Conv2d(args.ndf * 4, args.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(args.ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(args.ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
@@ -145,7 +160,7 @@ class Discriminator(nn.Module):
         return self.main(input)
 
 # Create the Discriminator
-netD = Discriminator(ngpu).to(device)
+netD = Discriminator(args.ngpu).to(device)
 
 # Apply  the weights_init function to init. weights to mean = 0, stdev = 0.2
 netD.apply(weightsInit)
@@ -157,15 +172,15 @@ print(netD)
 criterion = nn.BCELoss()
 
 # Create batch of latent vectors that we will use
-fixedNoise = torch.randn(64, nz, 1, 1, device=device)
+fixedNoise = torch.randn(64, args.nz, 1, 1, device=device)
 
 # labels
 realLabel = 1
 fakeLabel = 0
 
 # Setup the Adam optimizer for both networks
-optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
 
 # Training Loop
 img_list = []
@@ -174,7 +189,7 @@ D_losses = []
 iters = 0
 
 # For each epoch
-for epoch in range(numEpochs):
+for epoch in range(args.numEpochs):
     # For each batch in DataLoader
     for i, data in enumerate(dataloader, 0):
         #####
@@ -200,7 +215,7 @@ for epoch in range(numEpochs):
 
         ## Train with all-fake batch ##############
         # Generate batch of latent vectors
-        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        noise = torch.randn(b_size, args.nz, 1, 1, device=device)
 
         # Generate fake image batch with G
         fake = netG(noise)
@@ -240,7 +255,7 @@ for epoch in range(numEpochs):
         ## Show Losses
         if i % 50 == 0:
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                  % (epoch, numEpochs, i, len(dataloader),
+                  % (epoch, args.numEpochs, i, len(dataloader),
                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
         # Save Losses for plotting later
@@ -248,7 +263,7 @@ for epoch in range(numEpochs):
         D_losses.append(errD.item())
 
         # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == numEpochs-1) and (i == len(dataloader)-1)):
+        if (iters % 500 == 0) or ((epoch == args.numEpochs-1) and (i == len(dataloader)-1)):
             with torch.no_grad():
                 fake = netG(fixedNoise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
